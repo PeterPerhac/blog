feaf253b017e11764072ec7ba5e08f7f4cabc658
Peter Perhac <peter.perhac@digital.hmrc.gov.uk>
2017-05-29 19:02:47 +0100

    more work on cats article

---
+++ dem-codez/src/main/scala/com/perhac/blog/TraversingLater.scala
@@ -0,0 +1,33 @@
+package com.perhac.blog
+
+import cats.instances.{FutureInstances, ListInstances}
+import cats.syntax.TraverseSyntax
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration.Duration.Inf
+import scala.concurrent.{Await, Future}
+
+object TraversingLater extends TraverseSyntax with FutureInstances with ListInstances {
+
+  def f1() = Future {
+    Thread.sleep(100)
+    println(s"${System.currentTimeMillis()} I return Foo")
+    Some("Foo")
+  }
+
+  def f2() = Future {
+    Thread.sleep(50)
+    println(s"${System.currentTimeMillis()} No. Not here")
+    None
+  }
+
+  def f3() = Future {
+    println(s"${System.currentTimeMillis()} I return Bar")
+    Some("Bar")
+  }
+
+  val program = List(() => f1(), () => f2(), () => f3()).traverse(_ ()) map (_.flatten)
+
+  def main(args: Array[String]): Unit = Await.result(program map println, Inf)
+
+}
